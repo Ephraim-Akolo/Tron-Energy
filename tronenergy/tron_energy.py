4a4fd9e2d53d6f4f44ba902259aecc1cee51e9b0
Ephraim <ephraimakolo2017@gmail.com>
2024-09-01 00:46:21 +0100

    modified jsonify

--- tronenergy/tron_energy.py
+++ tronenergy/tron_energy.py
@@ -2,13 +2,14 @@ import requests
 import hmac
 import hashlib
 import json
+from urllib.parse import urljoin
 from time import time
 
 
 class TronEnergy(object):
     base_url = 'https://itrx.io/'
 
-    def __init__(self, api_key, api_secret):
+    def __init__(self, api_key:str, api_secret:str):
         self._api_secret = api_secret
 
         self.sess = requests.session()
@@ -22,6 +23,10 @@ class TronEnergy(object):
         return hmac.new(self._api_secret.encode(), message.encode(), hashlib.sha256).hexdigest()
     
     def jsonify(self, data:dict):
-        return json.dumps(data, sort_keys=True, separators=(',', ':'))
+        if data:
+            return json.dumps(data, sort_keys=True, separators=(',', ':'))
+        return ""
+    
+    
 
     
