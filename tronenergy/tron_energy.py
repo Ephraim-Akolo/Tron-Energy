a0d907f8e808c1a5c79d0de3f3066bcdb31aa9ff
Ephraim <ephraimakolo2017@gmail.com>
2024-08-31 23:55:44 +0100

    added init method

--- tronenergy/tron_energy.py
+++ tronenergy/tron_energy.py
@@ -5,6 +5,9 @@ from time import time
 class TronEnergy(object):
     base_url = 'https://itrx.io/'
 
+    def __init__(self, api_key, api_secret):
+        self._api_key = api_key
+        self._api_secret = api_secret
 
     def get_timestamp(self):
         return str(int(time()))
