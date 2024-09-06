c74ace7f3397f7c2de8c6bf384013806d46d83ac
Ephraim <ephraimakolo2017@gmail.com>
2024-09-06 18:24:31 +0100

    async api

---
+++ tron_energy/async_tron_energy.py
@@ -0,0 +1,35 @@
+import os
+import hashlib
+import hmac
+import json
+from aiohttp import ClientSession, ClientResponseError, ClientResponse
+from time import time
+from urllib.parse import urljoin
+
+
+TronAddress = str
+
+
+class AsyncTronEnergy:
+    base_url = 'https://itrx.io/'
+
+    def __init__(self, api_key:str=None, api_secret:str=None):
+        if not api_secret:
+            api_secret = os.getenv('TRON_ENERGY_API_SECRET')
+        if api_secret is None:
+            raise ValueError("API secret is required")
+        if not api_key:
+            api_key = os.getenv('TRON_ENERGY_API_KEY')
+        if api_key is None:
+            raise ValueError("API key is required")
+        
+        self._api_secret = str(api_secret)
+        self.sess = ClientSession(headers={
+            'Content-Type': 'application/json',
+            'API-KEY': api_key
+        })
+
+    async def close(self):
+        await self.sess.close()
+
+    
