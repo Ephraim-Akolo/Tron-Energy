490bf51b18cd260422536507e30b1151bdb5e8f0
Ephraim <ephraimakolo2017@gmail.com>
2024-09-07 00:40:41 +0100

    test list by number of transfers

--- tests/test_async_tron_energy.py
+++ tests/test_async_tron_energy.py
@@ -143,4 +143,53 @@ class TestTronEnergyMethods(unittest.IsolatedAsyncioTestCase):
         # Assert
         self.assertEqual(response, expected_response)
 
+    @patch('tron_energy.async_tron_energy.ClientSession.get')
+    async def test_list_purchases_by_number_of_transfers(self, mock_get):
+        # Arrange
+        expected_response = {
+            "count": 2,
+            "code": 0,
+            "page": 1,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "id": 2,
+                    "receive_address": "TEX5nLeFJ1dyazhJC3P9eYJs7hxgk7knJF",
+                    "status": 1,
+                    "last_step": 0,
+                    "main_delegated": False,
+                    "expired_time": None,
+                    "create_time": "2023-08-23T11:09:27.986610+08:00",
+                    "update_time": "2023-08-23T11:09:27.986660+08:00",
+                    "last_step_display": "deledate",
+                    "status_display": "enable",
+                    "auto_type": 1,                     # 1-only energy，2-energy+bandwidth
+                    "auto_type_display": "only energy",
+                    "max_energy": 65000,
+                    "period": 7,
+                    "count_limit": 8                    # times
+                },
+            ]
+        }
+        
+        # Mock the response object
+        mock_response = AsyncMock()
+        mock_response.status = 200
+        mock_response.json = AsyncMock(return_value=expected_response)
+
+        # Mock the context manager
+        mock_get.return_value.__aenter__.return_value = mock_response
+        mock_get.return_value.__aexit__.return_value = AsyncMock()
+
+        data = {
+            "receive_address": "TR7NHnXw5423f8j766h899234567890",
+        }
+    
+        # Act
+        response = await self.tron_energy.list_purchases_by_number_of_transfers(**data)
+    
+        # Assert
+        self.assertEqual(response, expected_response)
+
    
