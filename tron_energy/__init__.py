46b1c0db0e1927e27ee799eb24ed1201eb28ed5f
Ephraim <ephraimakolo2017@gmail.com>
2024-09-06 18:25:30 +0100

    add version to package init

--- tron_energy/__init__.py
+++ tron_energy/__init__.py
@@ -1,3 +1,15 @@
+import importlib
+
+def get_version():
+    try:
+        version_module = importlib.import_module('.version', package='tron_energy')
+        return version_module.version
+    except ImportError:
+        return "0.0.0"
+
+__version__ = get_version()
+
+
 from .tron_energy import TronEnergy
 from .async_tron_energy import AsyncTronEnergy
 
